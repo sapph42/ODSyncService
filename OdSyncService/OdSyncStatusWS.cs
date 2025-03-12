2238b0db5962e1486f7872fa40c527017b9f5813
sapph42 <31635854+sapph42@users.noreply.github.com>
2025-03-12 14:04:26 -0400

    Catch issues related to inaccessible registry keys

--- OdSyncService/OdSyncStatusWS.cs
+++ OdSyncService/OdSyncStatusWS.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Security.Principal;
 using System.DirectoryServices.AccountManagement;
 using OneDrive.Native;
 using OneDrive.Logging;
+using System.Security;
 
 #nullable enable
 namespace OneDrive.OdSyncService {
@@ -13,7 +14,9 @@ namespace OneDrive.OdSyncService {
         internal static bool OnDemandOnly { get; set; } = false;
         private static string UserSID {
             get {
-                userSID ??= WindowsIdentity.GetCurrent().User.ToString();
+                if (userSID is null) {
+                    userSID = WindowsIdentity.GetCurrent().User.ToString();
+                }
                 return userSID;
             }
         }
@@ -43,78 +46,88 @@ namespace OneDrive.OdSyncService {
 
         public IEnumerable<StatusDetail> GetStatusInternal() {
             const string subkeyString = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\SyncRootManager\";
-            using RegistryKey key = Registry.LocalMachine.OpenSubKey(subkeyString);
-            if (key is null) {
-                yield return new StatusDetail() { Status = ServiceStatus.OnDemandOrUnknown };
-            } else {
-                if (key.SubKeyCount == 0) {
-                    yield return new StatusDetail() { Status = ServiceStatus.OnDemandOrUnknown, ServiceType = "OneDrive" };
-                }
-                foreach (string subkey in key.GetSubKeyNames()) {
-                    RegistryKey displayKey = key.OpenSubKey(subkey);
-                    string? displayName = displayKey.GetValue("DisplayNameResource") as string;
-                    using RegistryKey userKey = key.OpenSubKey(String.Format("{0}{1}", subkey, @"\UserSyncRoots"));
-                    if (userKey != null && userKey.Name.Contains(UserSID)) {
-                        foreach (string valueName in userKey.GetValueNames()) {
-                            StatusDetail detail = new StatusDetail();
-                            try {
-                                SecurityIdentifier id = new SecurityIdentifier(valueName);
-                                //string userName = id.Translate(typeof(NTAccount)).Value;
-                                detail.UserName = id.Translate(typeof(NTAccount)).Value;
-                                detail.UserSID = valueName;
-                                detail.DisplayName = displayName;
-                                detail.SyncRootId = subkey;
+            StatusDetail detail = new StatusDetail();
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(subkeyString)) {
+                if (key is null) {
+                    yield return new StatusDetail() { Status = ServiceStatus.OnDemandOrUnknown };
+                } else {
+                    if (key.SubKeyCount == 0) {
+                        yield return new StatusDetail() { Status = ServiceStatus.OnDemandOrUnknown, ServiceType = "OneDrive" };
+                    }
+                    foreach (string subkey in key.GetSubKeyNames()) {
+                        try {
+                            detail = new StatusDetail();
+                            RegistryKey displayKey = key.OpenSubKey(subkey);
+                            string? displayName = displayKey.GetValue("DisplayNameResource") as string;
+                            using (RegistryKey userKey = key.OpenSubKey(String.Format("{0}{1}", subkey, @"\UserSyncRoots"))) {
+                                if (userKey != null && userKey.Name.Contains(UserSID)) {
+                                    foreach (string valueName in userKey.GetValueNames()) {
+                                        detail = new StatusDetail();
+                                        try {
+                                            SecurityIdentifier id = new SecurityIdentifier(valueName);
+                                            //string userName = id.Translate(typeof(NTAccount)).Value;
+                                            detail.UserName = id.Translate(typeof(NTAccount)).Value;
+                                            detail.UserSID = valueName;
+                                            detail.DisplayName = displayName;
+                                            detail.SyncRootId = subkey;
 
-                                string[] parts = userKey.Name.Split('!');
+                                            string[] parts = userKey.Name.Split('!');
 
-                                if (parts.Length > 1) {
-                                    detail.ServiceType = parts[Math.Min(2, parts.Length - 1)].Split('|')[0];
-                                } else {
-                                    detail.ServiceType = "INVALID";
+                                            if (parts.Length > 1) {
+                                                detail.ServiceType = parts[Math.Min(2, parts.Length - 1)].Split('|')[0];
+                                            } else {
+                                                detail.ServiceType = "INVALID";
+                                            }
+                                        } catch (Exception ex) {
+                                            detail.UserName = String.Format("{0}: {1}", ex.GetType().ToString(), ex.Message);
+                                            WriteLog.WriteErrorEvent("OneDrive " + detail.UserName);
+                                        }
+                                        detail.LocalPath = userKey.GetValue(valueName) as string;
+                                        detail.StatusString = GetStatus(detail.LocalPath!).ToString();
+                                    }
                                 }
-                            } catch (Exception ex) {
-                                detail.UserName = String.Format("{0}: {1}", ex.GetType().ToString(), ex.Message);
-                                WriteLog.WriteErrorEvent("OneDrive " + detail.UserName);
                             }
-                            detail.LocalPath = userKey.GetValue(valueName) as string;
-                            detail.StatusString = GetStatus(detail.LocalPath!).ToString();
-                            yield return detail;
+                        } catch (SecurityException) {
+                            detail = new StatusDetail() { Status = ServiceStatus.OnDemandOrUnknown };
                         }
+                        yield return detail;
                     }
                 }
             }
         }
         public IEnumerable<StatusDetail> GetStatusInternalGroove() {
-            const string subkeyString = @"Software\Microsoft\Office";
-            using RegistryKey key = Registry.CurrentUser.OpenSubKey(subkeyString);
-            if (key == null) {
-                yield return new StatusDetail() { Status = ServiceStatus.OnDemandOrUnknown, ServiceType = "Groove" };
-            } else {
-                if (key.SubKeyCount == 0) {
-                    yield return new StatusDetail() { Status = ServiceStatus.OnDemandOrUnknown };
-                }
-                foreach (string subkey in key.GetSubKeyNames()) {
-                    using RegistryKey userKey = key.OpenSubKey(String.Format("{0}{1}", subkey, @"\Common\Internet"));
-                    if (userKey != null && userKey.GetValue("LocalSyncClientDiskLocation") as String[] != null) {
-                        string[] folders = userKey.GetValue("LocalSyncClientDiskLocation") as String[] ?? new string[0];
-                        foreach (string folder in folders) {
-                            StatusDetail detail = new StatusDetail();
-                            try {
-                                detail.UserName = WindowsIdentity.GetCurrent().Name;
-                                detail.UserSID = UserPrincipal.Current.Sid.ToString();
-                                string[] parts = subkey.Split('!');
-                                detail.ServiceType = String.Format("Groove{0}", parts[parts.Length - 1]);
-                            } catch (Exception ex) {
-                                detail.UserName = String.Format("Groove - {0}: {1}", ex.GetType().ToString(),
-                                    ex.Message);
-                                Logging.WriteLog.WriteErrorEvent(detail.UserName);
-                            }
-                            detail.LocalPath = folder;
-                            detail.StatusString = GetStatus(detail.LocalPath).ToString();
-                            yield return detail;
-                        }
+            const string subkeyString = @"Software\Microsoft\Office"; 
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(subkeyString)) {
+                if (key == null) {
+                    yield return new StatusDetail() { Status = ServiceStatus.OnDemandOrUnknown, ServiceType = "Groove" };
+                } else {
+                    if (key.SubKeyCount == 0) {
+                        yield return new StatusDetail() { Status = ServiceStatus.OnDemandOrUnknown };
                     }
+                    foreach (string subkey in key.GetSubKeyNames()) {
+                        using (RegistryKey userKey = key.OpenSubKey(String.Format("{0}{1}", subkey, @"\Common\Internet"))) {
+                            if (userKey != null && userKey.GetValue("LocalSyncClientDiskLocation") as String[] != null) {
+                                string[] folders = userKey.GetValue("LocalSyncClientDiskLocation") as String[] ?? new string[0];
+                                foreach (string folder in folders) {
+                                    StatusDetail detail = new StatusDetail();
+                                    try {
+                                        detail.UserName = WindowsIdentity.GetCurrent().Name;
+                                        detail.UserSID = UserPrincipal.Current.Sid.ToString();
+                                        string[] parts = subkey.Split('!');
+                                        detail.ServiceType = String.Format("Groove{0}", parts[parts.Length - 1]);
+                                    } catch (Exception ex) {
+                                        detail.UserName = String.Format("Groove - {0}: {1}", ex.GetType().ToString(),
+                                            ex.Message);
+                                        Logging.WriteLog.WriteErrorEvent(detail.UserName);
+                                    }
+                                    detail.LocalPath = folder;
+                                    detail.StatusString = GetStatus(detail.LocalPath).ToString();
+                                    yield return detail;
+                                }
+                            }
+                        }
 
+                    }
                 }
             }
         }
@@ -124,6 +137,8 @@ namespace OneDrive.OdSyncService {
                 OneDrive.UacHelper.IsProcessElevated));
             StatusDetailCollection statuses = new StatusDetailCollection();
             foreach (StatusDetail status in GetStatusInternal()) {
+                if (status.SyncRootId is null)
+                    continue;
                 uint hr = API.GetStateBySyncRootId(status.SyncRootId, out OneDriveState state);
                 if (hr == 0) {
                     status.QuotaUsedBytes = state.UsedQuota;
